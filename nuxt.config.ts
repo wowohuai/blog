fcedcc85e04739a23b36278dfeecd965b1a38a0f
张祥 <502123065@qq.com>
2022-06-03 10:40:46 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  experimental: {
+    reactivityTransform: true,
+    viteNode: true,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+  typescript: {
+    typeCheck: true,
+  },
+})
